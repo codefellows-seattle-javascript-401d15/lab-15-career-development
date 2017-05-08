5ad130e5aa7acf9fb38db2a547438c8419edee0c
Brianna <briannaburrows@gmail.com>
2017-05-08 13:23:30 -0700

    adding bracket functionality

--- lab-brianna/bracket-tinder.js
+++ lab-brianna/bracket-tinder.js
@@ -4,9 +4,12 @@ const Stack = module.exports = require('./stack');
 
 let bracketArray = [];
 
-Stack.prototype.tinder = function (str) {
+Stack.prototype.tinder = function(str) {
   if(str.startsWith(']' || ')' || '}' )) err => {
     return console.error(err);
   };
-  
+  for (let i=0; i < str.length; i++) {
+    if(str[i]==='{') bracketArray.push(str[i]);
+
+  }
 };
