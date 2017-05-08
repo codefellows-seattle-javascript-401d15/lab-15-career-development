1fe617ceeb5d2ff031231cc7439cbd1392647ef7
Benjamin <ayzenbergb@gmail.com>
2017-05-08 09:37:45 -0700

    code done

---
+++ lab-ben/lib/bracket-lint.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Stack = require('./stack.js');
+
+//O(n)
+module.exports = function(string) {
+  let stack = new Stack();
+  string.split('').forEach(char => _check(char));
+
+  //if anything is on the stack after the check then fail
+  if(stack.head) return false;
+
+  return true;
+
+  function _check(char) {
+    if(char === '[' || char === '{' || char === '(' || char === ']' || char === '}' || char === ')') {
+      if(char === '[' || char === '{' || char === '(') {
+        //add to the stack push(char)
+        stack.push(char);
+        return;
+      }
+      if(stack.peek === '[' && char === ']') {
+        //remove from the stack pop()
+        stack.pop();
+        return;
+      }
+      if(stack.peek === '{' && char === '}') {
+        //remove from the stack pop()
+        stack.pop();
+        return;
+      }
+      if(stack.peek === '(' && char === ')') {
+        //remove from the stack pop()
+        stack.pop();
+        return;
+      }
+      return false;
+    }
+  }
+};
