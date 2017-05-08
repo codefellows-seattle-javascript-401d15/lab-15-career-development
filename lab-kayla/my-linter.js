96593e7b02540bc43dd446ba10b0643db6739a48
Kayla Asay <kdasay16@gmail.com>
2017-05-08 10:09:28 -0700

    if parameter

--- lab-kayla/my-linter.js
+++ lab-kayla/my-linter.js
@@ -1,9 +1,13 @@
 'use strict';
 
-function STR(str) {
+const Stack = module.exports = require('./stack');
+
+Stack.prototype.create = function STR(str) {
   let curly = new Stack();
   let bracket = new Stack();
   let parin = new Stack();
+
+  if(str.startsWith('}' || ']' || ')')) err => console.error(err);
 
   for(let i = 0; i < str.length; i++) {
     if(str[i]==='{') curly.push(str[i]);
@@ -11,12 +15,10 @@ function STR(str) {
     if(str[i]==='(') parin.push(str[i]);
 
     if(str[i]==='}') curly.pop;
-    if(str[i]==='}') bracket.pop;
-    if(str[i]==='}') parin.pop;
+    if(str[i]===']') bracket.pop;
+    if(str[i]===')') parin.pop;
   }
   if(curly) console.error;
   if(bracket) console.error;
   if(parin) console.error;
 }
-
-STR();
