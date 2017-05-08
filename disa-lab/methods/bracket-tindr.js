d3e94b00b95f7b159192ebb3b8cdb506668ad52a
Disa <justdisa@gmail.com>
2017-05-08 12:18:06 -0700

    removed demo code reference file

--- disa-lab/methods/bracket-tindr.js
+++ disa-lab/methods/bracket-tindr.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const Stack = module.exports = require('./stack');
+
+let bracketArray = []; //making an empty array
+
+Stack.prototype.tinder = function(str) {
+  if(str.startsWith(']' || '}' || ')') {
+    
+  }
+
+  })
+}
